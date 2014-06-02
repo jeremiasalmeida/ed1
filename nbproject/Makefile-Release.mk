a7a7d11f26e14f63a45923d1b51ee0f5c2b20566
careers <careers@brdlnb62v1.daitan.com>
2014-06-02 15:53:05 -0300

    start implementation of a avl tree for accounts

--- nbproject/Makefile-Release.mk
+++ nbproject/Makefile-Release.mk
@@ -35,7 +35,8 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/main.o
+	${OBJECTDIR}/main.o \
+	${OBJECTDIR}/samples.o
 
 
 # C Compiler Flags
@@ -66,6 +67,11 @@ ${OBJECTDIR}/main.o: main.c
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} $@.d
 	$(COMPILE.c) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/main.o main.c
+
+${OBJECTDIR}/samples.o: samples.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} $@.d
+	$(COMPILE.c) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/samples.o samples.c
 
 # Subprojects
 .build-subprojects:
